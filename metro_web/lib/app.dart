a3aab40aad567abf9d20471ef4a8c31266ec5a55
Murilo Molina Barone <murilo.m.barone@gmail.com>
2024-10-08 18:50:43 -0300

    padronizações e novos comentarios

--- metro_web/lib/app.dart
+++ metro_web/lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:metro_web/pages/homepage.dart';
+import 'package:metro_web/pages/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,17 +11,25 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primaryColor:  const Color.fromRGBO(0, 20, 137, 1), // Cor principal
         scaffoldBackgroundColor: Colors.white, // Cor de fundo do Scaffold
+        // Padronização da Appbar em todas as páginas
         appBarTheme: const AppBarTheme(
           color: Color.fromRGBO(0, 20, 137, 1), // Cor da AppBar
           iconTheme: IconThemeData(color: Colors.white), // Cor dos ícones na AppBar
           titleTextStyle: TextStyle(color: Colors.white, fontSize: 16.0) 
         ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
+        // padronização de todos os elevatedButton
+        elevatedButtonTheme: ElevatedButtonThemeData( 
           style: ElevatedButton.styleFrom(
             backgroundColor: const Color.fromRGBO(0, 20, 137, 1), // Cor padrão dos botões
             foregroundColor: Colors.white, // Cor do texto
             shadowColor: Colors.grey
           ),
+        ),
+        // padronização do drawer
+        drawerTheme: const DrawerThemeData(
+          backgroundColor: Colors.white, // Cor de fundo do drawer
+          elevation: 5, // Sombra do drawer
+          scrimColor: Colors.black54, // Cor da sombra atrás do drawer
         ),
         // Definir a fonte padrão aqui
         fontFamily: 'Roboto', // próxima a padrão do metro (helvetica)
