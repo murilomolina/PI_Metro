63187da7795e0d3f9173b061f4588cca9998e01f
Murilo Molina Barone <murilo.m.barone@gmail.com>
2024-10-13 01:19:08 -0300

    exemplo de estilo e placeholders da página de controle de usuarios

--- metro_web/lib/pages/controle_usuarios.dart
+++ metro_web/lib/pages/controle_usuarios.dart
@@ -1,29 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:metro_web/widgets/card_usuario.dart';
 import 'package:metro_web/widgets/custom_app_bar.dart';
 import 'package:metro_web/widgets/custom_drawer.dart';
 
 class ControleUsuarios extends StatelessWidget {
+  
   const ControleUsuarios({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(titulo: 'Controle de Usuários'),
-      drawer: CustomDrawer(pagType: 1), // 1 para páginas que não são a página principal
-      body: SingleChildScrollView(
+    return Scaffold(
+      appBar: const CustomAppBar(titulo: 'Controle de Usuários'), floatingActionButton: botaoAdd(context),
+      drawer: const CustomDrawer(pagType: 1), // 1 para páginas que não são a página principal
+      body: const SingleChildScrollView(
         scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Wrap( 
-              spacing: 8.0, 
-              runSpacing: 8.0, 
-              children: [
-                
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Wrap( 
+                spacing: 40.0, 
+                runSpacing: 40.0, 
+                children: [
+                  Cardusuario(usuario: 'Usuario1', funcao: 'ADM', ocupacao: 'Gerencia de Tecnologia'),
+                  Cardusuario(usuario: 'Usuario2', funcao: 'ADM', ocupacao: 'Gerencia de Tecnologia'),
+                  Cardusuario(usuario: 'Usuario3', funcao: 'ADM', ocupacao: 'Gerencia de Tecnologia'),
+                  Cardusuario(usuario: 'Usuario4', funcao: 'ADM', ocupacao: 'Gerencia de Tecnologia'),
+                  Cardusuario(usuario: 'Usuario5', funcao: 'ADM', ocupacao: 'Gerencia de Tecnologia'),
+                  Cardusuario(usuario: 'Usuario6', funcao: 'ADM', ocupacao: 'Gerencia de Tecnologia'),
+                  Cardusuario(usuario: 'Usuario7', funcao: 'ADM', ocupacao: 'Gerencia de Tecnologia'),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget botaoAdd(context){
+    return ElevatedButton(
+      onPressed: () {
+        _showAddUserDialog(context);
+      },
+      style: ElevatedButton.styleFrom(
+        shape: const CircleBorder(), 
+        padding: const EdgeInsets.all(20), 
+        backgroundColor: Theme.of(context).primaryColor, 
+        shadowColor: Colors.black.withOpacity(0.3), 
+        elevation: 10, 
+      ),
+      child: const Icon(
+        Icons.add, 
+        size: 40, 
+        color: Colors.white, 
+      ),
+    );
+  }
+  
+  // Método para mostrar o diálogo de adição de usuário
+  void _showAddUserDialog(BuildContext context) {
+    final TextEditingController usuarioController = TextEditingController();
+    final TextEditingController senhaController = TextEditingController();
+    final TextEditingController ocupacaoController = TextEditingController();
+    final TextEditingController funcaoController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Adicionar Usuário'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: usuarioController,
+                  decoration: const InputDecoration(labelText: 'Nome de Usuário'),
+                ),
+                TextField(
+                  controller: senhaController,
+                  decoration: const InputDecoration(labelText: 'Senha'),
+                  obscureText: true, // Esconder o texto da senha
+                ),
+                TextField(
+                  controller: ocupacaoController,
+                  decoration: const InputDecoration(labelText: 'Ocupação'),
+                ),
+                TextField(
+                  controller: funcaoController,
+                  decoration: const InputDecoration(labelText: 'Função'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // implementar a lógica de adicionar o usuário (futuramente)
+                print("Usuário: ${usuarioController.text}, Senha: ${senhaController.text}, Ocupação: ${ocupacaoController.text}, Função: ${funcaoController.text}");
+                Navigator.of(context).pop();
+              },
+              child: const Text('Adicionar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
---
+++ metro_web/lib/widgets/card_usuario.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+class Cardusuario extends StatelessWidget {
+  final String usuario;
+  final String funcao;
+  final String ocupacao;
+
+  const Cardusuario({super.key, required this.usuario, required this.funcao, required this.ocupacao});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 157, 193, 255),
+              Theme.of(context).primaryColor,
+            ], // Gradiente de fundo
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+          borderRadius: BorderRadius.circular(15), 
+        ),
+        width: 250,
+        height: 220,
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              usuario,
+              style: const TextStyle(
+                fontSize: 24, 
+                fontWeight: FontWeight.bold,
+                color: Colors.white, 
+              ),
+            ),
+            Text(
+              ocupacao,
+              style: const TextStyle(
+                fontSize: 16, 
+                color: Colors.black, 
+              ),
+            ),
+            Text(
+              funcao,
+              style: const TextStyle(
+                fontSize: 14, 
+                fontWeight: FontWeight.bold,
+                color: Colors.black, 
+              ),
+            ),
+            // Botões de Editar e Excluir
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit, color: Colors.white),
+                  onPressed: () {
+                    _showEditDialog(context);
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete, color: Colors.white),
+                  onPressed: () {
+                    _showDeleteConfirmationDialog(context);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showEditDialog(BuildContext context) {
+    final TextEditingController usuarioController = TextEditingController(text: usuario);
+    final TextEditingController funcaoController = TextEditingController(text: funcao);
+    final TextEditingController ocupacaoController = TextEditingController(text: ocupacao);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Editar Usuário'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: usuarioController,
+                decoration: const InputDecoration(labelText: 'Usuário'),
+              ),
+              TextField(
+                controller: funcaoController,
+                decoration: const InputDecoration(labelText: 'Função'),
+              ),
+              TextField(
+                controller: ocupacaoController,
+                decoration: const InputDecoration(labelText: 'Ocupação'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // implementar a lógica de salvar as alterações
+                print("Usuário: ${usuarioController.text}, Função: ${funcaoController.text}, Ocupação: ${ocupacaoController.text}");
+                Navigator.of(context).pop();
+              },
+              child: const Text('Salvar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDeleteConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmar Exclusão'),
+          content: const Text('Tem certeza que deseja excluir este usuário?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // implementar a lógica de exclusão do usuário
+                print("Usuário excluído: $usuario");
+                Navigator.of(context).pop();
+              },
+              child: const Text('Excluir'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
