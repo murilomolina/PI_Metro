9ac4c051123c6ca5684809dff13912cc7f191906
Murilo Molina Barone <murilo.m.barone@gmail.com>
2024-10-16 17:31:24 -0300

    novo tipo de implementação de gerador de qr code, com todos os parametros dos equipamentos

--- metro_web/lib/pages/qrcode_generator.dart
+++ metro_web/lib/pages/qrcode_generator.dart
@@ -15,7 +15,6 @@ class QRcodeGenerator extends StatefulWidget {
   const QRcodeGenerator({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _QRcodeGeneratorState createState() => _QRcodeGeneratorState();
 }
 
@@ -25,10 +24,19 @@ class _QRcodeGeneratorState extends State<QRcodeGenerator> {
   final TextEditingController equipamentoIdController = TextEditingController();
   final TextEditingController dataValidadeController = TextEditingController();
   final TextEditingController localizacaoController = TextEditingController();
-  final TextEditingController localizacaoDetalhadaController = TextEditingController();
+  final TextEditingController observacoesController = TextEditingController();
+  final TextEditingController patrimonioController = TextEditingController(); 
+  final TextEditingController capacidadeController = TextEditingController(); 
+  final TextEditingController codigo_fabricanteController = TextEditingController(); 
+  final TextEditingController data_fabricacaoController = TextEditingController(); 
+  final TextEditingController ultima_recargaController = TextEditingController(); 
+  final TextEditingController proxima_inspecaoController = TextEditingController(); 
+  final TextEditingController statusController = TextEditingController(); 
+  final TextEditingController id_localizacaoController = TextEditingController(); 
 
   // Variável para armazenar o QR Code
   GlobalKey qrKey = GlobalKey();
+  bool isSimpleMode = false; // Controle para o modo simples
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +44,7 @@ class _QRcodeGeneratorState extends State<QRcodeGenerator> {
       appBar: const CustomAppBar(titulo: 'Gerador de QR Code'),
       drawer: const CustomDrawer(pagType: 1),
       body: Center(
-        child: SingleChildScrollView( // Permitir rolagem se necessário
+        child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
@@ -48,47 +56,142 @@ class _QRcodeGeneratorState extends State<QRcodeGenerator> {
                 ),
                 const SizedBox(height: 20),
                 
+                // Switch para alternar entre o modo completo e simples
+                SwitchListTile(
+                  activeTrackColor: Theme.of(context).primaryColor,
+                  title: const Text('Modo Simples (ID e Patrimônio)', textAlign: TextAlign.end, style: TextStyle(fontWeight: ui.FontWeight.bold),),
+                  value: isSimpleMode,
+                  onChanged: (value) {
+                    setState(() {
+                      isSimpleMode = value;
+                    });
+                  },
+                ),
+                
                 // Campos de entrada
-                TextField(
-                  controller: tipoEquipamentoController,
-                  decoration: const InputDecoration(
-                    labelText: 'Tipo de Equipamento',
-                    border: OutlineInputBorder(),
+                if (!isSimpleMode) ...[
+                  TextField(
+                    controller: tipoEquipamentoController,
+                    decoration: const InputDecoration(
+                      labelText: 'Tipo de Equipamento',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: equipamentoIdController,
-                  decoration: const InputDecoration(
-                    labelText: 'ID do Equipamento',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: equipamentoIdController,
+                    decoration: const InputDecoration(
+                      labelText: 'ID do Equipamento',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: dataValidadeController,
-                  decoration: const InputDecoration(
-                    labelText: 'Data de Validade',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: dataValidadeController,
+                    decoration: const InputDecoration(
+                      labelText: 'Data de Validade',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: localizacaoController,
-                  decoration: const InputDecoration(
-                    labelText: 'Localização',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: localizacaoController,
+                    decoration: const InputDecoration(
+                      labelText: 'Localização',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  controller: localizacaoDetalhadaController,
-                  decoration: const InputDecoration(
-                    labelText: 'Localização Detalhada',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: observacoesController,
+                    decoration: const InputDecoration(
+                      labelText: 'Observações',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: patrimonioController,
+                    decoration: const InputDecoration(
+                      labelText: 'Patrimônio',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: capacidadeController,
+                    decoration: const InputDecoration(
+                      labelText: 'Capacidade',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: codigo_fabricanteController,
+                    decoration: const InputDecoration(
+                      labelText: 'Código do Fabricante',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: data_fabricacaoController,
+                    decoration: const InputDecoration(
+                      labelText: 'Data de Fabricação',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: ultima_recargaController,
+                    decoration: const InputDecoration(
+                      labelText: 'Última Recarga',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: proxima_inspecaoController,
+                    decoration: const InputDecoration(
+                      labelText: 'Próxima Inspeção',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: statusController,
+                    decoration: const InputDecoration(
+                      labelText: 'Status',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: id_localizacaoController,
+                    decoration: const InputDecoration(
+                      labelText: 'ID da Localização',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                ] else ...[
+                  TextField(
+                    controller: equipamentoIdController,
+                    decoration: const InputDecoration(
+                      labelText: 'ID do Equipamento',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: patrimonioController,
+                    decoration: const InputDecoration(
+                      labelText: 'Patrimônio',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                ],
                 
                 // Botão para gerar QR Code
                 ElevatedButton(
@@ -100,11 +203,15 @@ class _QRcodeGeneratorState extends State<QRcodeGenerator> {
                 const SizedBox(height: 20),
                 
                 // Exibindo o QR Code se os dados forem preenchidos
-                if (tipoEquipamentoController.text.isNotEmpty &&
+                if ((isSimpleMode && 
+                    equipamentoIdController.text.isNotEmpty && 
+                    patrimonioController.text.isNotEmpty) ||
+                    (!isSimpleMode && 
+                    tipoEquipamentoController.text.isNotEmpty && 
                     equipamentoIdController.text.isNotEmpty &&
                     dataValidadeController.text.isNotEmpty &&
                     localizacaoController.text.isNotEmpty &&
-                    localizacaoDetalhadaController.text.isNotEmpty)
+                    observacoesController.text.isNotEmpty))
                   Column(
                     children: [
                       QrImageView(
@@ -130,11 +237,24 @@ class _QRcodeGeneratorState extends State<QRcodeGenerator> {
 
   // Método para gerar a string do QR Code
   String _generateQrData() {
-    return 'Tipo de Equipamento: ${tipoEquipamentoController.text}\n'
-           'ID: ${equipamentoIdController.text}\n'
-           'Data de Validade: ${dataValidadeController.text}\n'
-           'Localização: ${localizacaoController.text}\n'
-           'Localização Detalhada: ${localizacaoDetalhadaController.text}';
+    if (isSimpleMode) {
+      return 'ID: ${equipamentoIdController.text}\n'
+             'Patrimônio: ${patrimonioController.text}';
+    } else {
+      return 'Tipo de Equipamento: ${tipoEquipamentoController.text}\n'
+             'ID: ${equipamentoIdController.text}\n'
+             'Data de Validade: ${dataValidadeController.text}\n'
+             'Localização: ${localizacaoController.text}\n'
+             'Observações: ${observacoesController.text}\n'
+             'Patrimônio: ${patrimonioController.text}\n'
+             'Capacidade: ${capacidadeController.text}\n'
+             'Código do Fabricante: ${codigo_fabricanteController.text}\n'
+             'Data de Fabricação: ${data_fabricacaoController.text}\n'
+             'Última Recarga: ${ultima_recargaController.text}\n'
+             'Próxima Inspeção: ${proxima_inspecaoController.text}\n'
+             'Status: ${statusController.text}\n'
+             'ID da Localização: ${id_localizacaoController.text}';
+    }
   }
 
   // Método para salvar o QR Code
@@ -151,7 +271,7 @@ class _QRcodeGeneratorState extends State<QRcodeGenerator> {
 
         // Obter o diretório para salvar a imagem
         final directory = await getExternalStorageDirectory();
-        final String path = '${directory!.path}/qrcode$tipoEquipamentoController-$equipamentoIdController.png';
+        final String path = '${directory!.path}/qrcode_${tipoEquipamentoController.text}_${equipamentoIdController.text}.png';
         
         // Criar o arquivo e salvar a imagem
         File imgFile = File(path);
@@ -170,9 +290,34 @@ class _QRcodeGeneratorState extends State<QRcodeGenerator> {
     } else {
       // Exibir um snackbar se a permissão não for concedida
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Permissão para acessar armazenamento não concedida.')),
+        const SnackBar(content: Text('Permissão de armazenamento negada')),
       );
     }
   }
 
+  // Método para processar apenas ID e Patrimônio
+  void _processIdAndPatrimonio() {
+    String id = equipamentoIdController.text;
+    String patrimonio = patrimonioController.text;
+
+  // aqui nessa função minha intenção é fazer uma pesquisa no banco de dados usando como parametros o id e o patrimonio e o resultado da pesquisa (todos os outros parametros do extintor) serão inseridos no QRCode.
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Dados Processados'),
+          content: Text('ID: $id\nPatrimônio: $patrimonio'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
