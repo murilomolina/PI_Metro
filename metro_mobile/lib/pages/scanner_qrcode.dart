2f3df4625a5de2a751cc8be51d91c9e3dc2e97ff
Murilo Molina Barone <murilo.m.barone@gmail.com>
2024-10-22 18:53:01 -0300

    trata erro de Url que não pode ser acessada

--- metro_mobile/lib/pages/scanner_qrcode.dart
+++ metro_mobile/lib/pages/scanner_qrcode.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:metro_mobile/widgets/custom_appbar.dart';
 import 'package:metro_mobile/widgets/custom_drawer.dart';
 import 'package:qr_bar_code_scanner_dialog/qr_bar_code_scanner_dialog.dart';
@@ -53,7 +54,7 @@ class _ScannerQrcodeState extends State<ScannerQrcode> {
 
    // Função para tratar e exibir os dados do QR Code
   Widget _handleQRCode(String code) {
-    if (Uri.tryParse(code)?.hasAbsolutePath == true) {
+    if (Uri.tryParse(code)?.hasAbsolutePath == true || RegExp(r"^https:\/\/\w+\.com$").hasMatch(code)) {
       // Se for uma URL, mostrar botão para abrir no navegador
       return Column(
         children: [
@@ -63,7 +64,9 @@ class _ScannerQrcodeState extends State<ScannerQrcode> {
           ),
           const SizedBox(height: 10),
           ElevatedButton(
-            onPressed: () => _launchURL(code),
+            onPressed: () { 
+                _launchURL(code, context);
+            },
             child: const Text("Abrir URL"),
           ),
         ],
@@ -72,9 +75,11 @@ class _ScannerQrcodeState extends State<ScannerQrcode> {
       try {
         var jsonData = jsonDecode(code);
         // Se for JSON, exibir em uma lista de chave-valor
+        print("Json data\n"+jsonData);
         return _chaveValor(jsonData);
       } catch (e) {
         // Se for texto comum, exibir como texto simples
+        print("Texto comum\n"+code);
         return Padding(
           padding: const EdgeInsets.all(10.0),
           child: Text(
@@ -115,11 +120,83 @@ class _ScannerQrcodeState extends State<ScannerQrcode> {
   }
 
   // Função para abrir URL no navegador
-  Future<void> _launchURL(String url) async {
+  Future<void> _launchURL(String url, BuildContext context) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
-      throw 'Não foi possível abrir $url';
+       // ignore: use_build_context_synchronously
+       _trataErroUrl(url, context);
     }
+  }
+
+  // Função para tratar o erro e exibir o diálogo
+  Future<void> _trataErroUrl(String code, BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15), 
+          ),
+            title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Row(
+                children: [
+                  Icon(Icons.error_outline, color: Colors.red),
+                  Text(' Erro'),
+                ],
+              ),
+              IconButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 153, 153, 153),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop(); // Fechar o pop-up
+              },
+              icon: const Icon(Icons.close, size: 18),
+            ),
+            ],
+          ),
+          content: SingleChildScrollView(
+            child: IntrinsicHeight(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Não foi possível abrir a URL."),
+                  const SizedBox(height: 10),
+                  Text("URL: $code"),
+                ],
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () {
+                      Clipboard.setData(ClipboardData(
+                          text: code)); // Copiar URL para área de transferência
+                      Navigator.of(context).pop(); // Fechar o diálogo
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text("URL copiada para a área de transferência!")),
+                      );
+                    },
+                  icon: const Icon(Icons.copy, size: 18),
+                  label: const Text('Copiar Url'),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
   }
 }
